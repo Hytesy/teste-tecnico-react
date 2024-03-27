7b1145138b3109f54d0225769fda436a2c91d2da
Lucas Kaíque <lucas.kaique175@hotmail.com>
2024-03-27 14:30:18 +0000

    Refactor PokemonList component and update dependencies

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -1,69 +1,40 @@
-import { useEffect, useState } from "react"
-import { getNames, getPokemon, getPokemonsPerType } from "../services/requestApi"
-import { LoadLessPokemon, LoadMorePokemon } from "../components/pokemonQuantityButtons";
+import React, { useState } from "react";
+import {
+  LoadLessPokemon,
+  LoadMorePokemon,
+} from "../components/pokemonQuantityButtons";
 import TypesFilter from "../components/typesFilter";
 import List from "../components/pokemonList";
 import { Footer, Header } from "../components/HeadFooter";
+import usePokemonData from "../hooks/usePokemonData ";
 
 const PokemonList = () => {
-  const counter = 10
-  const [pokemonList, setPokemonList] = useState()
-  const [pokemonCount, setPokemonCount] = useState(counter)
-  const [filter, setFilter] = useState('')
+  const [typeSelected, setTypeSelected] = useState("");
+  const { pokemonsFiltered, loadMorePokemons, selectType } = usePokemonData();
 
-  useEffect(() => {
-    async function fetchData() {
-      const listName = await getNames(pokemonCount)
-      const pokemons = await getPokemon(listName)
-      setPokemonList(pokemons)
-    }
-      fetchData()
- 
-  }, [pokemonCount])
-  
-  useEffect(() => {
-    async function fetchFiltredData(type) {
-      const listName = await getPokemonsPerType(type)
-      const pokemons = await getPokemon(listName)
-      setPokemonList(pokemons)
-    }
-    if (!filter) {
-      fetchData()
-    } else {
-      fetchFiltredData(filter)
-    }
-  }, [filter])
+  console.log(pokemonsFiltered.length > 0 ? pokemonsFiltered : pokemonsFiltered);
 
-
-
-  const loadMorePokemon = () => {
-    setPokemonCount(prevCount => prevCount + counter)
-  }
-  const loadLessPokemon = () => {
-    if (pokemonCount > counter) {
-      setPokemonCount(prevCount => prevCount - counter)
-    }
-  }
-
-  const handleFilerChange = (event) => {
-    setFilter(event.target.value)
-  }
+  const handleFilerChange = (type) => {
+    selectType(type);
+    setTypeSelected(type);
+  };
 
   return (
     <>
       <Header />
       <main>
-        <TypesFilter value={filter} onChange={handleFilerChange} />
-        <List pokemons={pokemonList} />
+        <TypesFilter value={typeSelected} onChange={handleFilerChange} />
+        {/* <List
+          pokemons={pokemonsFiltered.length > 0 ? pokemonsFiltered : pokemons}
+        /> */}
         <div>
-          <LoadLessPokemon onClick={loadLessPokemon} />
-          <LoadMorePokemon onClick={loadMorePokemon} />
+          <LoadLessPokemon onClick={loadMorePokemons} />
+          <LoadMorePokemon onClick={loadMorePokemons} />
         </div>
       </main>
-      <Footer/>
-
+      <Footer />
     </>
   );
 };
 
-export default PokemonList
+export default PokemonList;
